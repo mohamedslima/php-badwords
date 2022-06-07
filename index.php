176cd9e5f521b9be5ccd6844363200c95d07f901
mohamedslima <hammaslima@gmail.com>
2022-06-07 15:38:18 +0200

    completed

--- index.php
+++ index.php
@@ -13,6 +13,27 @@ $user_text = "Lasciatemi cantare con la chitarra in mano lasciatemi cantare sono
 <p>
     <?php 
     $text_length = strlen($user_text);
-    echo "La stringa è lunga" . $text_length . "caratteri";
+    echo "La stringa è lunga" . " " . $text_length . " " . "caratteri";
+    ?>
+</p>
+
+<?php 
+$badword = $_GET['badword'];
+$censored = str_replace($badword, "***", $user_text);
+?>
+
+<h2>Paragrafo con parola censurata</h2>
+<p>
+    <?php 
+    echo $censored;
+    ?>
+     <!-- ad esewmpio http://localhost/php-badwords/?badword=sit -->
+</p>
+<h2>
+    Lunghezza del mio paragrafo con censura
+</h2>
+<p>
+    <?php 
+    echo "La stringa censurata è lunga" . " " . $text_length . " " . "caratteri";
     ?>
 </p>
